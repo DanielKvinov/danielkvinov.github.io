51cfb3ae8cd2ac35df484b8bed80a86b76d387c6
Daniel Kvinov <69066367+DanielKvinov@users.noreply.github.com>
2021-01-28 13:59:32 +0200

    * black yet

--- portfolio/js/interactive.js
+++ portfolio/js/interactive.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
 
-	$(".projects-list__cell").click(function(){
-		$(".projects-cell").toggleClass("projects-cell__open");
-	});
+	// $(".projects-list__cell").click(function(){
+	// 	$(".projects-cell").toggleClass("projects-cell__open");
+	// });
 
 	// Кастомный скроллбар
 	$("body").mCustomScrollbar({
 		theme: "light-3",
+	});
+
+	$('.feedback-slider').slick({
+	  dots: true,
+	  speed: 700,
 	});
 
 });
