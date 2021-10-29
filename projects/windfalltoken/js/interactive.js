708880c9275d1a64a646164175bbb2509d3a30ea
Daniel Kvinov <danielkvinov@gmail.com>
2021-10-30 00:30:53 +0300

    Media

---
+++ projects/windfalltoken/js/interactive.js
@@ -0,0 +1,7 @@
+let $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+	$hamburger.toggleClass("is-active");
+	// $(".menu-mobile__nav").slideToggle();
+	$(".menu-mobile__nav").toggleClass("mobile-menu__visible");
+	$("body, .windfall-background").toggleClass("overflow-hidden");
+});
