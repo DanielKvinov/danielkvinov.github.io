3a83ed1f5469a4da120093a66e69b56f3c69f0bb
Daniel Kvinov <danielkvinov@gmail.com>
2021-10-30 18:51:31 +0300

    !

--- projects/windfalltoken/js/interactive.js
+++ projects/windfalltoken/js/interactive.js
@@ -10,10 +10,3 @@ $(".menu-mobile__nav-container a").click(function(){
 	$(".menu-mobile__nav").toggleClass("mobile-menu__visible");
 	$("body, .windfall-background").toggleClass("overflow-hidden");
 });
-
-$("document").ready(function(){
-	// 100vh and chrome toolbar fix
-	let vh = window.innerHeight * 0.01;
-	// Then we set the value in the --vh custom property to the root of the document
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
